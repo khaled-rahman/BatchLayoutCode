8de9381c14737d3990c772298206e8a76df11de6
Majedul Sujon <majed.sujon@gmail.com>
2019-10-04 16:10:32 -0400

    fixed

--- optimization/dsumsc_naive_ompM.c
+++ optimization/dsumsc_naive_ompM.c
@@ -4,11 +4,11 @@
 void ATL_USUMSC(long M, const double *X, long N, const double *Y, double *Z, long incX, long incY )
 {
    long i, j;
-   #pragma omp parallel for simd schedule(static) 
    for (j=0; j < N; j++)
    {
       double y = Y[j]; 
       double sum=0.0;
+      #pragma omp parallel for simd reduction(+:sum)
       for (i=0; i < M; i++)
          sum += y * X[i];
       Z[j] = sum;
