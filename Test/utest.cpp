fa194a68c7a99a471fd84a121f4475ba2a75abab
Md. Khaledur Rahman <khaled.cse.07@gmail.com>
2019-10-29 00:09:57 -0400

    Revert "Msdev"

--- Test/utest.cpp
+++ Test/utest.cpp
@@ -78,77 +78,25 @@ void uTestAlgorithms(char *argv[]){
 
 void uTestNewAlgo(int argc, char *argv[]){
 	CSR<INDEXTYPE, VALUETYPE> A_csr;
-        //string inputfile = "./datasets/input/jagmesh1.mtx";
-	 string inputfile = "./datasets/input/3elt_dual.mtx";
-	//string inputfile = "./datasets/input/skirt.mtx";
+        string inputfile = "./datasets/input/jagmesh1.mtx";
         string outputdir = "./datasets/output/";
         SetInputMatricesAsCSR(A_csr, inputfile);
         A_csr.Sorted();
         vector<VALUETYPE> outputvec;
-   #if 0
-        algorithms algo = algorithms(A_csr, inputfile, outputdir, 0, 1, 1.2, "");
-	algo.cacheBlockingminiBatchForceDirectedAlgorithm(500, 48, 256, 0);	
-        algorithms algo2 = algorithms(A_csr, inputfile, outputdir, 0, 1, 1.2, "");
+	algorithms algo = algorithms(A_csr, inputfile, outputdir, 0, 1, 1.2, "");
+	algo.cacheBlockingminiBatchForceDirectedAlgorithm(500, 48, 256, 0);
+	algorithms algo2 = algorithms(A_csr, inputfile, outputdir, 0, 1, 1.2, "");
 	algo2.cacheBlockingminiBatchForceDirectedAlgorithmSD(500, 48, 256, 0);
-        test(algo.nCoordinates, algo2.nCoordinates, A_csr.rows);	
-   #endif	
+	test(algo.nCoordinates, algo2.nCoordinates, A_csr.rows);	
+	
 	newalgo na = newalgo(A_csr, inputfile, outputdir, 0, 1, 1.2, "");
-	na.EfficientVersionUnRoll(5, 48, 256);
-        
-        newalgo na2 = newalgo(A_csr, inputfile, outputdir, 0, 1, 1.2, "");
+	newalgo na2 = newalgo(A_csr, inputfile, outputdir, 0, 1, 1.2, "");
 	na2.EfficientVersion(5, 48, 256);	
+	na.EfficientVersionUnRoll(5, 48, 256);
 }
-
-
-void GetAvgTimes(int argc, char *argv[], int nrep)
-{
-   double time1=0.0, time2=0.0; 
-   double energy1=0.0, energy2=0.0; 
-   CSR<INDEXTYPE, VALUETYPE> A_csr;
-   //string inputfile = "./datasets/input/jagmesh1.mtx";
-   //string inputfile = "./datasets/input/3elt_dual.mtx";
-   string inputfile = "./datasets/input/pkustk01.mtx";
-   //string inputfile = "./datasets/input/skirt.mtx";
-   string outputdir = "./datasets/output/";
-   SetInputMatricesAsCSR(A_csr, inputfile);
-   A_csr.Sorted();
-   vector<VALUETYPE> outputvec;
-   
-   newalgo na = newalgo(A_csr, inputfile, outputdir, 0, 1, 1.2, "");
-   newalgo na2 = newalgo(A_csr, inputfile, outputdir, 0, 1, 1.2, "");
-
-   for (int i=0; i < nrep; i++)
-   {
-      outputvec = na.EfficientVersion(5, 48, 256);
-      time1 += outputvec[1];
-      energy1 += (outputvec[0]/nrep); // to avoid overflow 
-      //cout << "1st energy=" << outputvec[0] << "  Time = " << outputvec[1] <<endl; 
-
-      outputvec = na2.EfficientVersionUnRoll(5, 48, 256);
-      //cout << "2nd energy=" << outputvec[0] << " Time = " << outputvec[1] << endl; 
-      time2 += outputvec[1];
-      energy2 += (outputvec[0]/nrep); // to avoid overflow 
-   }
-   time1 /= nrep;
-   time2 /= nrep;
-   cout << "Efficient version: Avg time"<<"("<<nrep<<") = " << time1 <<endl;
-   cout << "Efficient version: Avg Energy"<<"("<<nrep<<") = " << energy1 <<endl;
-   cout << "Efficient Unroll version: Avg time"<<"("<<nrep<<") = " << time2 <<endl;
-   cout << "Efficient version: Avg Energy"<<"("<<nrep<<") = " << energy2 <<endl;
-   cout << "Speedup of Unroll version = " << time1/time2 << endl;
-
-}
-
-#define DEBUG 0 
-
 int main(int argc, char* argv[]){
 	
 	//uTestAlgorithms(argv);
-        //uTestNewAlgo(argc, argv);
-   #if DEBUG == 1 
-        GetAvgTimes(argc, argv, 1);
-   #else
-        GetAvgTimes(argc, argv, 20);
-   #endif
+        uTestNewAlgo(argc, argv);
 	return 0;
 }
